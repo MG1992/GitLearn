3cc138fa37cf78b70f878410fcfb52e9d52a7928
Josh Guan <josh.guan@atomintl.com>
2019-12-05 23:23:03 +0800

    生成冲突

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const app = new Koa();
 app.use(async (ctx, next) => {
   console.log(ctx.headers);
   console.log('我真的是个傻子...');
+  console.log('我可能真的是个傻子...');
   await next();
 });
 app.use(async ctx => {
