040023be7050a773ee1424424685fdf4d4a9a170
Josh Guan <josh.guan@atomintl.com>
2019-12-05 22:10:42 +0800

    测试两个文件

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 const Koa = require('koa');
 const app = new Koa();
+
+app.use(async (ctx, next) => {
+  console.log(ctx.headers);
+  await next();
+});
 app.use(async ctx => {
   ctx.body = 'Hello World';
 });
+
+
 
 app.listen(3000);
